e2f2b2b1dca5b5ca0ef9f4477839a893926dca74
sdj7060 <sdj7060@nate.com>
2020-11-04 17:55:42 +0900

    HomeComing첫번째

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World..!!");
+		System.out.println("안뇽?? Java..!!");
+		System.out.println("코로나19 물러가라..!!");
+	}
+}
+
+ 
